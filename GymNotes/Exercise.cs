fe4f5ef0f4a8c536359b754129c5a4a8055d8a1d
Den <denis.a.govorov@gmail.com>
2016-12-08 15:50:29 +0200

    First class + test proj added

---
+++ GymNotes/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymNotes
+{
+    public class Exercise
+    {
+        public string Name;
+        public enum ExcerciseType { Weighted, Unweigted, Distance }
+        public ExcerciseType Type;
+        public string Instruction;
+        public enum MuscleGroup { Back, Abs, Biceps, Chest, Forearm, Glutes, LowerLeg, Shoulder, Triceps, UpperLeg }
+        public List<MuscleGroup> EnvolvedGroups = new List<MuscleGroup>();
+    }
+}
